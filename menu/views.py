44d79b24f1ed5f0a5fd65e801770022bf0d8458f
Ivanna Parfeniuk <parfeniuk.ivannaa@gmail.com>
2026-01-20 21:56:56 +0200

    Add comments

--- menu/views.py
+++ menu/views.py
@@ -4,6 +4,22 @@ from .models import MEAL_CATEGORY, MenuItem
 
 
 class MenuItemListView(generic.ListView):
+    """
+    MenuItemListView - клас-представлення для відображення списку елементів меню.
+
+    - Наслідується від generic.ListView, що дозволяє автоматично обробляти запити на отримання списку об'єктів.
+    - queryset: визначає набір даних, який буде відображатися, впорядкований за датою створення у спадному порядку.
+    - template_name: вказує на шаблон, який використовується для рендерингу сторінки зі списком елементів меню.
+
+    Методи:
+        get_context_data(self, **kwargs):
+            - Перевизначає стандартний метод для додавання додаткових даних у контекст шаблону.
+            - Додає до контексту список категорій страв (MEAL_CATEGORY) під ключем "meals".
+            - Повертає розширений контекст для використання у шаблоні.
+
+    Призначення:
+        Даний клас використовується для відображення сторінки зі списком страв меню, а також для передачі додаткової інформації про категорії страв у шаблон.
+    """
     queryset = MenuItem.objects.order_by("-date_created")
     template_name = "menu_item_list.html"
 
@@ -15,6 +31,6 @@ class MenuItemListView(generic.ListView):
         return context
 
 
-class MenuItemDetailView(generic.DeleteView):
+class MenuItemDetailView(generic.DetailView):
     model = MenuItem
     template_name = "menu_item_detail.html"
